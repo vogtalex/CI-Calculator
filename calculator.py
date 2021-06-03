a423f4b46018a9bf89052fc38ca2dbc5867e6a4b
Alex Vogt <vogtalex@oregonstate.edu>
2021-06-02 21:10:54 -0700

    fixed linting errors

--- calculator.py
+++ calculator.py
@@ -9,3 +9,7 @@ def add(a, b):
 
 def subtract(a, b):
     return a - b
+
+
+def multiply(a, b):
+    return a * b
--- test_calculator.py
+++ test_calculator.py
@@ -5,11 +5,11 @@ import calculator
 
 
 class TestCalculatorApp:
-	def test_add(self):
-		assert 5 == calculator.add(3,2)
+    def test_add(self):
+        assert 5 == calculator.add(3, 2)
 
-	def test_subtract(self):
-		assert 5 == calculator.subtract(10,5)
+    def test_subtract(self):
+        assert 5 == calculator.subtract(10, 5)
 
-	def test_multiply(self):
-		assert 4 == calculator.multiply(2,2)
+    def test_multiply(self):
+        assert 4 == calculator.multiply(2, 2)
