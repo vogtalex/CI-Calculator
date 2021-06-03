d608788a1ca4647fab856cadebe902ec49bd8867
Alex Vogt <vogtalex@oregonstate.edu>
2021-06-02 20:52:36 -0700

    added config file for circle ci

--- calculator.py
+++ calculator.py
@@ -2,8 +2,10 @@
 Calculator app using circle ci.
 """
 
-def add(a,b):
-	return a + b
 
-def subtract(a,b):
-	return a - b
+def add(a, b):
+    return a + b
+
+
+def subtract(a, b):
+    return a - b
